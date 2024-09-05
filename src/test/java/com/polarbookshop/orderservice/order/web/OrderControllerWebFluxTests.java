e64008a11f0a3861b9cb3123dd04e449d993cb8a
Seine <>
2024-09-06 00:42:47 +0900

    20240905 update test

--- src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
@@ -4,11 +4,12 @@ import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
 import com.polarbookshop.orderservice.order.domain.OrderStatus;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -25,15 +26,13 @@ class OrderControllerWebFluxTests {
     @Test
     void whenBookNotAvailableThenRejectOrder() {
         var orderRequest = new OrderRequest("1234567890", 3);
-        var expectedOrder = OrderService.buildRejectedOrder(
-                orderRequest.isbn(), orderRequest.quantity());
-        given(orderService.submitOrder(
-                orderRequest.isbn(), orderRequest.quantity())
-        ).willReturn(Mono.just(expectedOrder));
+        var expectedOrder = OrderService.buildRejectedOrder(orderRequest.isbn(), orderRequest.quantity());
+        given(orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity()))
+                .willReturn(Mono.just(expectedOrder));
 
         webClient
                 .post()
-                .uri("/orders/")
+                .uri("/orders")
                 .bodyValue(orderRequest)
                 .exchange()
                 .expectStatus().is2xxSuccessful()
